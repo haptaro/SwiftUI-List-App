634c5231efb28194370128dd41629e4885d4dfab
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-24 23:04:54 -0700

    :art: add Staging Project

---
+++ Staging/Staging/StagingApp.swift
@@ -0,0 +1,18 @@
+//
+//  StagingApp.swift
+//  Staging
+//
+//  Created by Kotaro Fukuo on 2021/08/24.
+//
+
+import SwiftUI
+import Body
+
+@main
+struct StagingApp: App {
+  var body: some Scene {
+    WindowGroup {
+      BodyView()
+    }
+  }
+}
