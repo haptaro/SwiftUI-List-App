cb8bbf586fa7ab7701b9d589f5ed00fbcba41695
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-24 23:10:27 -0700

    :art: add Production Xcode Project

---
+++ Production/Production/ProductionApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProductionApp.swift
+//  Production
+//
+//  Created by Kotaro Fukuo on 2021/08/24.
+//
+
+import SwiftUI
+import Body
+
+@main
+struct ProductionApp: App {
+  var body: some Scene {
+    WindowGroup {
+      BodyView()
+    }
+  }
+}
